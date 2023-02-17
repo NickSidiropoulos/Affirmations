6af93c655cd9a359f835115814192534bcc83535
Nick Sdr <nick93sid@users.noreply.github.com>
2023-02-17 16:16:09 +0200

    Initial commit

---
+++ app/src/main/java/com/nicksidiropoulos/affirmations/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.nicksidiropoulos.affirmations
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.service.autofill.Dataset
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.nicksidiropoulos.affirmations.adapter.ItemAdapter
+import com.nicksidiropoulos.affirmations.data.Datasource
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val myDataSet = Datasource().loadAffirmations()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataSet)
+        recyclerView.setHasFixedSize(true)
+
+
+    }
+}
